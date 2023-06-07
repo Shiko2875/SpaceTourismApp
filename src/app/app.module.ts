cf7c0c880954c7938ea587e15f42f8f0bcb1ca27
Shiko2875 <ssp2875@gmail.com>
2023-06-07 15:42:39 -0400

    Created necessary components: top-nav, home-page, destination, crew, and technology.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { DestinationComponent } from './destination/destination.component';
+import { CrewComponent } from './crew/crew.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    DestinationComponent,
+    CrewComponent,
+    TechnologyComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
