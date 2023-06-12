f535ecd8cb860f42bd079953546b5c39c6c814c4
Shiko2875 <ssp2875@gmail.com>
2023-06-12 15:26:10 -0400

    Updated top-nav and home-page components so that there is no background image overlapping. Also added routes to app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './home-page/home-page.component';
+import { DestinationComponent } from './destination/destination.component';
+import { CrewComponent } from './crew/crew.component';
+import { TechnologyComponent } from './technology/technology.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent},
+  { path: 'destination', component: DestinationComponent},
+  { path: 'crew', component: CrewComponent},
+  { path: 'tech', component: TechnologyComponent},
+  { path: '**', redirectTo: '/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
