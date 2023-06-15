d5f6c0e037bddd7c482b243b2b29e924c328b5b3
Shiko2875 <ssp2875@gmail.com>
2023-06-15 11:06:56 -0400

    Added Nav Bar for mobile view

--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-nav',
   templateUrl: './top-nav.component.html',
   styleUrls: ['./top-nav.component.css']
 })
-export class TopNavComponent {
+export class TopNavComponent implements OnInit {
 
+  ngOnInit(): void {
+    const menuBtn = document.querySelector('.hamburger');
+	  const mobileMenu = document.querySelector('.mobile-nav');
+
+	  menuBtn?.addEventListener('click', function () {
+		  menuBtn.classList.toggle('is-active');
+		  mobileMenu?.classList.toggle('is-active');
+	  });
+  }
+  
 }
